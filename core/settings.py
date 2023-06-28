b954e7a23952f6f1189cf33128a4fcfb8743d2db
igor.kulish <igor.kulish09@gmail.com>
2023-06-29 02:31:50 +0300

    Django init

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-l@5r-u8efm5wl$ctp4#(+i&zms4#z(+xi%-j*yo&5kb38jsh$q"
+# SECRET_KEY = "django-insecure-l@5r-u8efm5wl$ctp4#(+i&zms4#z(+xi%-j*yo&5kb38jsh$q"
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-l@5r-u8efm5wl$ctp4#(+i&zms4#z(+xi%-j*yo&5kb38jsh$q")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "polls",
+    #"polls.apps.PollsConfig",
 ]
 
 MIDDLEWARE = [
