9175c19e13f273e42c5e0ad8965698762446ea34
igor.kulish09 <igor.kulish09@gmail.com>
2023-07-07 23:33:39 +0300

    test CI/CD

--- catalog/models.py
+++ catalog/models.py
@@ -9,9 +9,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
-    class Meta:
-        verbose_name = "Question"
-        verbose_name_plural = "Questions"
+
 
     def __str__(self):
         return self.question_text
@@ -31,9 +29,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    class Meta:
-        verbose_name = "Choice"
-        verbose_name_plural = "Choices"
 
     def __str__(self):
         return self.choice_text
